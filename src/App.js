3282b424e28a4f83d70065e771448c2dea4e4984
gitsmin <sendex485@gmail.com>
2021-05-29 21:01:20 +0900

    practice map2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 // import React from "react";
+URL = "https://screamfmlondon.files.wordpress.com/2016/03/20160112_195618.jpg";
 
 const foodILike = [
   { name: "Kimchi", image: URL },
   { name: "Samgyeopsal", image: URL },
 ];
-URL = "http://xxx.xxx.com/xxx.jpg";
+// URL = "http://xxx.xxx.com/xxx.jpg";
 
 function Food({ name, picture }) {
   return (
     <div>
       <h1>I like {name}</h1>
-      <img src={picture} />
+      <img src={picture} width="300px" />
     </div>
   );
 }
