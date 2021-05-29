38575a1f3209ec5d86d58b919899eec357a00ea3
gitsmin <sendex485@gmail.com>
2021-05-29 17:21:24 +0900

    practice_map

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 // import React from "react";
 
+const foodILike = [
+  { name: "Kimchi", image: URL },
+  { name: "Samgyeopsal", image: URL },
+];
+URL = "http://xxx.xxx.com/xxx.jpg";
+
+function Food({ name, picture }) {
+  return (
+    <div>
+      <h1>I like {name}</h1>
+      <img src={picture} />
+    </div>
+  );
+}
+
 function App() {
-  return <div className="App" />;
-  // return <div>Hello</div>;
+  // return <div className="App" />;
+  // return <div>Home</div>;
+  return (
+    <div>
+      {foodILike.map((dish) => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
