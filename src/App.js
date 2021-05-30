1d660fbec0cc18a536bf65a09f18efe904e9e58d
gitsmin <sendex485@gmail.com>
2021-05-30 13:29:48 +0900

    2.4 PropTypes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 // import React from "react";
+import PropTpyes from "prop-types";
+
 URL = "https://screamfmlondon.files.wordpress.com/2016/03/20160112_195618.jpg";
 
 const foodILike = [
-  { name: "Kimchi", image: URL },
-  { name: "Samgyeopsal", image: URL },
+  { id: 1, name: "Kimchi", image: URL, rating: 2 },
+  { id: 2, name: "Samgyeopsal", image: URL, rating: 3 },
 ];
 // URL = "http://xxx.xxx.com/xxx.jpg";
 
-function Food({ name, picture }) {
+function Food({ name, picture, rating }) {
   return (
     <div>
       <h1>I like {name}</h1>
       <img src={picture} width="300px" />
+      <div>{rating}/10</div>
     </div>
   );
 }
@@ -22,10 +25,21 @@ function App() {
   return (
     <div>
       {foodILike.map((dish) => (
-        <Food name={dish.name} picture={dish.image} />
+        <Food
+          key={dish.id}
+          name={dish.name}
+          picture={dish.image}
+          rating={dish.rating}
+        />
       ))}
     </div>
   );
 }
+
+Food.propTypes = {
+  name: PropTpyes.string.isRequired,
+  picture: PropTpyes.string.isRequired,
+  rating: PropTpyes.number.isRequired,
+};
 
 export default App;
