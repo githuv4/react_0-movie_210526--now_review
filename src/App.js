382e41e4396bb7281eb211b33d648bec66b64ca8
gitsmin <sendex485@gmail.com>
2021-06-01 20:29:37 +0900

    3.0 Class Component ans state

--- src/App.js
+++ src/App.js
@@ -1,45 +1,25 @@
-// import React from "react";
-import PropTpyes from "prop-types";
+import React from "react";
 
-URL = "https://screamfmlondon.files.wordpress.com/2016/03/20160112_195618.jpg";
-
-const foodILike = [
-  { id: 1, name: "Kimchi", image: URL, rating: 2 },
-  { id: 2, name: "Samgyeopsal", image: URL, rating: 3 },
-];
-// URL = "http://xxx.xxx.com/xxx.jpg";
-
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h1>I like {name}</h1>
-      <img src={picture} width="300px" />
-      <div>{rating}/10</div>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
+  add = () => {
+    console.log("add");
+  };
+  minus = () => {
+    console.log("minus");
+  };
+  render() {
+    return (
+      <div>
+        <h1>A class {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>minus</button>
+        
+      </div>
+    );
+  }
 }
-
-function App() {
-  // return <div className="App" />;
-  // return <div>Home</div>;
-  return (
-    <div>
-      {foodILike.map((dish) => (
-        <Food
-          key={dish.id}
-          name={dish.name}
-          picture={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
-}
-
-Food.propTypes = {
-  name: PropTpyes.string.isRequired,
-  picture: PropTpyes.string.isRequired,
-  rating: PropTpyes.number.isRequired,
-};
 
 export default App;
