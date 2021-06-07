a19f8bead099b2a29bd1399bc40bafb83839e541
gitsmin <sendex485@gmail.com>
2021-06-07 19:02:23 +0900

    string slice

--- src/Movie.js
+++ src/Movie.js
@@ -6,18 +6,21 @@ function Movie({ year, title, summary, poster, genres }) {
   return (
     <div className="movie">
       <img src={poster} alt={title} title={title} />
-      <h4 className="movie__title">{title}</h4>
-      <h5 className="movie__year">{year}</h5>
-      <ul className="movie__genres genres">
-        {genres.map((genre, index) => (
-          <li key={index} className="genres__genre">
-            {index == genres.length - 1 ? genre : `${genre}/`}
-          </li>
-        ))}
-      </ul>
-      <p className="movie__summary">
-        {summary.length > 150 ? `${summary.substring(0, 150)}...` : summary}
-      </p>
+      <div className="content">
+        <h4 className="movie__title">{title}</h4>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="movie__genres genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres__genre">
+              {index == genres.length - 1 ? genre : `${genre}/`}
+            </li>
+          ))}
+        </ul>
+        <p className="movie__summary">
+          {/* {summary.length > 150 ? `${summary.substring(0, 250)}...` : summary} */}
+          {summary.length > 150 ? `${summary.slice(0, 250)}...` : summary}
+        </p>
+      </div>
     </div>
   );
 }
