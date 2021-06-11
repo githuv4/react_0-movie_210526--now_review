2331366614fd983492624ada599e617bad1c1b3b
gitsmin <sendex485@gmail.com>
2021-06-12 00:04:17 +0900

    6.1 building the router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
 
 function App() {
-  return <span>starting routing</span>;
+  return (
+    <HashRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/about" component={About} />
+    </HashRouter>
+  );
 }
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -10,7 +10,7 @@ function Movie({ year, title, summary, poster, genres }) {
         <h4 className="movie__title">{title}</h4>
         <h5 className="movie__year">{year}</h5>
         <ul className="movie__genres genres">
-          {genres.map((genre, index) => (
+          {genres?.map((genre, index) => (
             <li key={index} className="genres__genre">
               {index === genres.length - 1 ? genre : `${genre}/`}
             </li>
