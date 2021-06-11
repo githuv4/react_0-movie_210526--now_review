37b76c8d81879a3e9abb40e57116b7219659858f
gitsmin <sendex485@gmail.com>
2021-06-12 07:20:16 +0900

    6.2 Building the navigaition

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
+// import "./App.css";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <HashRouter>
+      <Navigation />
       <Route path="/" exact component={Home} />
       <Route path="/about" component={About} />
     </HashRouter>
--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 function About() {
-  return <div>About page</div>;
+  return (
+    <div className="about">
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, a in!
+        Odit architecto ad corporis delectus qui, fuga temporibus ex labore
+        harum, minus adipisci voluptatem quae sapiente commodi non praesentium
+        est fugit maiores numquam, similique asperiores explicabo quod
+        doloribus? Officia vero molestiae possimus, repellendus sunt cumque,
+        tempora deleniti obcaecati ipsam soluta, dicta modi recusandae error!
+        Quae odit hic sequi deserunt.
+      </p>
+    </div>
+  );
 }
 
 export default About;
